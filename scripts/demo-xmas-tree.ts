e3537a2833d43108a8b19a3b4d657d24ab435b44
michaelessiet <emsaa2002@gmail.com>
2024-01-15 16:26:50 +0100

    added chalk to demo-xmas-tree

--- scripts/demo-xmas-tree.ts
+++ scripts/demo-xmas-tree.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env -S pkgx bun
+import chalk from 'chalk'
 
 function printChristmasTree(height: number): void {
 	for (let i = 1; i <= height; i++) {
 		let spaces = " ".repeat(height - i);
 		let stars = "*".repeat(i * 2 - 1);
-		console.log(spaces + stars);
+		console.log(spaces + chalk.green(stars));
 	}
 }
 
